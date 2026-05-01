3142db11edb8a6ad107bd73096c7ac3e81fa3ff7
mistryneel24 <mistry.neel24@gmail.com>
2026-05-01 09:54:02 +0530

    Point frontend to Render backend

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+import os
 import requests
 import streamlit as st
 
 # =============================
 # CONFIG
 # =============================
-API_BASE = "http://127.0.0.1:8000"
+# API_BASE = "http://127.0.0.1:8000"
+API_BASE = os.getenv("API_BASE","https://project-2-0-33xg.onrender.com")
 TMDB_IMG = "https://image.tmdb.org/t/p/w500"
 
 GENRE_OPTIONS = [
